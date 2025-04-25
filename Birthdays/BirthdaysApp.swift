12fde9c83704077f262db9e825f43575e2fe9dc2
Lily Mosisa <lilymosisa13@gmail.com>
2025-04-24 20:39:45 -0400

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Lily Mosisa on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
