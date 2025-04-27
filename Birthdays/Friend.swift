f59db8113ecfbfebb8bfca198298958312f3e4cf
Lily Mosisa <lilymosisa13@gmail.com>
2025-04-27 17:36:50 -0400

    Added friend class with a list of friends and a datePicker

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,16 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Lily Mosisa on 4/24/25.
+//
+
+import Foundation
+class Friend {
+    var name : String
+    var birthday : Date
+    init(name: String, birthday: Date){
+        self.name = name
+        self.birthday = birthday
+    }
+}
